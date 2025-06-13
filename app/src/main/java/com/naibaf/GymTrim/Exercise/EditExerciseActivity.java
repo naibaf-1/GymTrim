c180a38ffc1f85b5158ef82be5d857cb33a9191e
naibaf-1 <Roland.Fabian@pm.me>
2025-06-13 22:27:08 +0200

    Added Apache 2.0 license to the header of each class.

--- app/src/main/java/com/naibaf/GymTrim/Exercise/EditExerciseActivity.java
+++ app/src/main/java/com/naibaf/GymTrim/Exercise/EditExerciseActivity.java
@@ -1,3 +1,17 @@
+/**  Copyright 2025 Fabian Roland (naibaf-1)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. **/
+
 package com.naibaf.GymTrim.Exercise;
 
 import android.app.Activity;
@@ -325,10 +339,3 @@ public class EditExerciseActivity extends AppCompatActivity {
         exercise_data.close();
     }
 }
-
-//FixMe: Picture just white
-
-//Todo: On Save: Edit all exercises in Plans too => Find all (Insert Id of this into PlanDB too) => Update it
-
-//Todo: Add graph with volume here
-//Todo: Add graph with weight/distance/time here
