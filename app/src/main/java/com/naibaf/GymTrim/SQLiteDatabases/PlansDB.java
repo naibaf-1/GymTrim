200461ab74b2d16f6705c49cc2f6083ade3a3398
naibaf-1 <naibaf-1@users.noreply.github.com>
2026-02-18 12:49:15 +0100

    Code cleanup: More comments and better arrangement
    Signed-off-by: naibaf-1 <naibaf-1@users.noreply.github.com>

--- app/src/main/java/com/naibaf/GymTrim/SQLiteDatabases/PlansDB.java
+++ app/src/main/java/com/naibaf/GymTrim/SQLiteDatabases/PlansDB.java
@@ -37,6 +37,9 @@ public class PlansDB extends SQLiteOpenHelper {
         DB = this.getWritableDatabase();
     }
 
+    //*******************************************************************************
+    // Create and Update the DB
+    //*******************************************************************************
     @Override
     public void onCreate(SQLiteDatabase DB) {
         try {
@@ -48,7 +51,6 @@ public class PlansDB extends SQLiteOpenHelper {
             e.printStackTrace();
         }
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
         DB.execSQL(" drop Table if exists PlansDetails ");
@@ -60,74 +62,13 @@ public class PlansDB extends SQLiteOpenHelper {
         DB.execSQL(" create Table ExerciseTable (tableMainId INTEGER primary key autoincrement, RecordWeight REAL, RecordDistance REAL, RecordSentences INTEGER, RecordTime NUMERIC, RowNumber INTEGER, RowIsDone INTEGER, TableId INTEGER, FOREIGN KEY (TableId) REFERENCES Exercise(exerciseMainId) ON DELETE CASCADE ) ");
     }
 
-    //Delete data
-    public void deletePlanData(int plansId) {
-        //Delete the plan itself
-        Cursor cursor = DB.rawQuery(" Select * From PlansDetails Where plansId = ? ", new String[]{String.valueOf(plansId)});
-        DB.delete("PlansDetails", "plansId = ?", new String[]{String.valueOf(plansId)});
+    //*******************************************************************************
+    // Helper functions for insertion, deletion, ...
+    //*******************************************************************************
 
-        //rowId.close();
-        cursor.close();
-    }
-
-    //Get all plans and their data from PlansDetails
-    public Cursor getAllPlans(){
-        // Convert dd-mm-yyyy to yyyy-mm-dd so that the plans will be sorted correctly
-        Cursor planData = DB.rawQuery(" Select * From PlansDetails Order By substr(DateOfLastTraining, 7, 4) || '-' || substr(DateOfLastTraining, 4, 2) || '-' || substr(DateOfLastTraining, 1, 2) DESC", null);
-        return planData;
-    }
-
-    //Delete exercise
-    public Boolean deleteExerciseData(int exerciseId) {
-        //Select the row
-        Cursor cursor = DB.rawQuery(" Select * From Exercise Where exerciseMainId = ? ", new String[]{String.valueOf(exerciseId)});
-        long result = DB.delete("Exercise", "exerciseMainId = ?", new String[]{String.valueOf(exerciseId)});
-        if (cursor.getCount() > 0) {
-            cursor.close();
-            //If Failed: false
-            return result != -1;
-        } else {
-            cursor.close();
-            return false;
-        }
-    }
-
-    //Get data of a specific exercise
-    public Cursor getBasicExerciseData(int exerciseId){
-        Cursor exerciseData = DB.rawQuery(" Select * From PlansDetails p INNER JOIN Exercise e ON p.plansId = e.ExerciseID Where e.exerciseMainId = ? ", new String[]{String.valueOf(exerciseId)});
-        return exerciseData;
-    }
-    public int addSingleExercise(int plansId, String notes, String name, Bitmap image, int repetitions, int weight, int time, int distance, int order, String date, int idInEDB){
-        //Insert data correctly
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("NameOfExercise", name);
-        contentValues.put("ImageOfExercise", CommonFunctions.getBitmapAsArray(image));
-        contentValues.put("NotesForExercise", notes);
-        contentValues.put("ExerciseID", plansId);
-        contentValues.put("ExerciseOrder", order);
-        contentValues.put("RecordRepetitions", repetitions);
-        contentValues.put("RecordDistance", distance);
-        contentValues.put("RecordWeight", weight);
-        contentValues.put("RecordTime", time);
-        contentValues.put("DateOfLastTraining", date);
-        contentValues.put("ExerciseIdInEDB", idInEDB);
-
-        long check = DB.insert("Exercise", null, contentValues);
-        return (int) check;
-    }
-
-    //Get exercise Id
-    public Cursor getExerciseId(int plansId){
-        Cursor data = DB.rawQuery(" Select exerciseMainId From Exercise  WHERE ExerciseID = ?  ORDER BY ExerciseOrder", new String[]{String.valueOf(plansId)});
-        return data;
-    }
-
-    //Get id of certain rows
-    public Cursor getRowId(int exerciseId){
-        Cursor data = DB.rawQuery(" Select tableMainId From ExerciseTable  WHERE TableId = ?  ORDER BY RowNumber", new String[]{String.valueOf(exerciseId)});
-        return data;
-    }
-
+    //-------------------------------------------------------------------------------
+    // Plan
+    //-------------------------------------------------------------------------------
     //Insert everything at once
     public int insertNewPlan(){
         ContentValues contentValue = new ContentValues();
@@ -135,81 +76,13 @@ public class PlansDB extends SQLiteOpenHelper {
         long plansId = DB.insert("PlansDetails", null, contentValue);
         return (int) plansId;
     }
-
-    //Get all data of an specific plan at once
-    public Cursor getAllPlanExercises(int plansId){
-        Cursor data = DB.rawQuery(" Select * From PlansDetails p INNER JOIN Exercise e ON p.plansId = e.ExerciseID WHERE p.plansId = ? Order By e.ExerciseOrder", new String[]{String.valueOf(plansId)});
-        return data;
+    // Get all plans and their data from PlansDetails
+    public Cursor getAllPlans(){
+        // Convert dd-mm-yyyy to yyyy-mm-dd so that the plans will be sorted correctly
+        Cursor planData = DB.rawQuery(" Select * From PlansDetails Order By substr(DateOfLastTraining, 7, 4) || '-' || substr(DateOfLastTraining, 4, 2) || '-' || substr(DateOfLastTraining, 1, 2) DESC", null);
+        return planData;
     }
-    public Cursor getBasicPlanData(int plansId){
-        Cursor data = DB.rawQuery(" Select * From PlansDetails WHERE plansId = ?", new String[]{String.valueOf(plansId)});
-        return data;
-    }
-    public Boolean updateTrainingData(int planId, String planName, String planNotes, String date, String timerDuration){
-        ContentValues contentvalues = new ContentValues();
-        contentvalues.put("NameOfPlan", planName);
-        contentvalues.put("NotesForPlan", planNotes);
-        contentvalues.put("DateOfLastTraining", date);
-        contentvalues.put("TimerDurationWhenTrainingLeft", timerDuration);
-        DB.update("PlansDetails", contentvalues, "plansId = ?", new String[]{String.valueOf(planId)});
-
-        return true;
-    }
-    public int addExerciseTableRow(int exerciseId, float weight, int repetitions, float time, float distance, int order) {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("RecordWeight", weight);
-        contentValues.put("RecordDistance", distance);
-        contentValues.put("RecordTime", time);
-        contentValues.put("RecordSentences", repetitions);
-        contentValues.put("TableId", exerciseId);
-        contentValues.put("RowNumber", order);
-        contentValues.put("RowIsDone", 0);
-        long check = DB.insert("ExerciseTable", null, contentValues);
-        return (int) check;
-    }
-    public Cursor getExerciseTableRows(int exerciseId){
-        Cursor rows = DB.rawQuery(" SELECT * FROM Exercise e INNER JOIN ExerciseTable t ON e.exerciseMainId = t.TableId WHERE t.TableId = ? ORDER BY t.RowNumber", new String[]{String.valueOf(exerciseId)});
-        return rows;
-    }
-    public void updateExerciseTableRows(int exerciseId, float weight, int repetitions, float time, float distance, int order){
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("RecordWeight", weight);
-        contentValues.put("RecordDistance", distance);
-        contentValues.put("RecordTime", time);
-        contentValues.put("RecordSentences", repetitions);
-        contentValues.put("TableId", exerciseId);
-        DB.execSQL("UPDATE ExerciseTable SET RecordWeight = " + weight + " , RecordDistance = " + distance + " , RecordTime = " + time+ ", RecordSentences = " + repetitions + " , TableId = " + exerciseId + " WHERE TableId = " + exerciseId + " AND RowNumber = " + order);
-    }
-    public void updateRowDone(int tableRowId, Boolean rowIsDone){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues(1);
-        contentValues.put("RowIsDone", CommonFunctions.getIntegerValueOfBoolean(rowIsDone));
-        db.update("ExerciseTable", contentValues, "tableMainId = ?", new String[]{String.valueOf(tableRowId)});
-    }
-
-    //Note when a specific exercise was trained before
-    public void setDateOfCurrentTrainingForExercise(int exerciseId, String date){
-        ContentValues contentValues = new ContentValues(1);
-        contentValues.put("DateOfLastTraining", date);
-        DB.update("Exercise", contentValues, "exerciseMainId = ?", new String[]{String.valueOf(exerciseId)});
-    }
-
-    //Get when a specific exercise was trained before
-    public String getDateOfExercise(int exerciseID){
-        Cursor date = DB.rawQuery("SELECT DateOfLastTraining FROM Exercise WHERE exerciseMainId = ?", new String[]{String.valueOf(exerciseID)});
-        date.moveToFirst();
-        int columnIndex = date.getColumnIndex("DateOfLastTraining");
-        return date.getString(columnIndex);
-    }
-
-    //Delete row
-    public void deleteExerciseTableRow(int rowId){
-        //Select the row
-        DB.rawQuery(" Select * From ExerciseTable Where tableMainId = ? ", new String[]{String.valueOf(rowId)});
-        DB.delete("ExerciseTable", "tableMainId = ?", new String[]{String.valueOf(rowId)});
-    }
-
-    //Functions for fast auto-save
+    // Editing functions
     public void editPlanName(String newName, int plansId){
         ContentValues contentValues = new ContentValues(1);
         contentValues.put("NameOfPlan", newName);
@@ -230,20 +103,68 @@ public class PlansDB extends SQLiteOpenHelper {
         contentValues.put("ReminderTime", duration);
         DB.update("PlansDetails", contentValues, "plansId = ?", new String[]{String.valueOf(plansId)});
     }
-    public void completelyRemoveEntireData() {
-        DB.delete("PlansDetails", null, null);
-        DB.delete("Exercise", null, null);
-        DB.delete("ExerciseTable", null, null);
-        DB.close();
+    public int addSingleExercise(int plansId, String notes, String name, Bitmap image, int repetitions, int weight, int time, int distance, int order, String date, int idInEDB){
+        //Insert data correctly
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NameOfExercise", name);
+        contentValues.put("ImageOfExercise", CommonFunctions.getBitmapAsArray(image));
+        contentValues.put("NotesForExercise", notes);
+        contentValues.put("ExerciseID", plansId);
+        contentValues.put("ExerciseOrder", order);
+        contentValues.put("RecordRepetitions", repetitions);
+        contentValues.put("RecordDistance", distance);
+        contentValues.put("RecordWeight", weight);
+        contentValues.put("RecordTime", time);
+        contentValues.put("DateOfLastTraining", date);
+        contentValues.put("ExerciseIdInEDB", idInEDB);
+
+        long check = DB.insert("Exercise", null, contentValues);
+        return (int) check;
     }
+    // Get all data of a specific plan at once
+    public Cursor getAllPlanExercises(int plansId){
+        Cursor data = DB.rawQuery(" Select * From PlansDetails p INNER JOIN Exercise e ON p.plansId = e.ExerciseID WHERE p.plansId = ? Order By e.ExerciseOrder", new String[]{String.valueOf(plansId)});
+        return data;
+    }
+    public Cursor getBasicPlanData(int plansId){
+        Cursor data = DB.rawQuery(" Select * From PlansDetails WHERE plansId = ?", new String[]{String.valueOf(plansId)});
+        return data;
+    }
+    // Delete a plan
+    public void deletePlanData(int plansId) {
+        //Delete the plan itself
+        Cursor cursor = DB.rawQuery(" Select * From PlansDetails Where plansId = ? ", new String[]{String.valueOf(plansId)});
+        DB.delete("PlansDetails", "plansId = ?", new String[]{String.valueOf(plansId)});
+
+        //rowId.close();
+        cursor.close();
+    }
+
+    //-------------------------------------------------------------------------------
+    // Overlay showing further information about an exercise
+    //-------------------------------------------------------------------------------
+    // Get exercise Id
+    public Cursor getExerciseId(int plansId){
+        Cursor data = DB.rawQuery(" Select exerciseMainId From Exercise  WHERE ExerciseID = ?  ORDER BY ExerciseOrder", new String[]{String.valueOf(plansId)});
+        return data;
+    }
+    // Get data of a specific exercise
+    public Cursor getBasicExerciseData(int exerciseId){
+        return DB.rawQuery(" Select * From PlansDetails p INNER JOIN Exercise e ON p.plansId = e.ExerciseID Where e.exerciseMainId = ? ", new String[]{String.valueOf(exerciseId)});
+    }
+    // Get id of certain rows
+    public Cursor getRowId(int exerciseId){
+        Cursor data = DB.rawQuery(" Select tableMainId From ExerciseTable  WHERE TableId = ?  ORDER BY RowNumber", new String[]{String.valueOf(exerciseId)});
+        return data;
+    }
+    // Get the id of an exercise in EDB in order to load changes by the editor
     public int getIdOfExerciseInEDB(int exerciseId){
         Cursor ids = DB.rawQuery("SELECT ExerciseIdInEDB FROM Exercise WHERE exerciseMainId = ?", new String[]{String.valueOf(exerciseId)});
         ids.moveToFirst();
         int columnIndexOfId = ids.getColumnIndex("ExerciseIdInEDB");
         return ids.getInt(columnIndexOfId);
     }
-
-    //Update exercises too when in editor changed
+    // Update exercises too when in editor changed
     public Boolean checkIfEditedExerciseExistsInPDB(int idOfEditedInEDB){
         Cursor check = DB.rawQuery("Select ExerciseIdInEDB FROM Exercise WHERE ExerciseIdInEDB = ?", new String[]{String.valueOf(idOfEditedInEDB)});
         if (check.getCount() != 0){
@@ -287,7 +208,96 @@ public class PlansDB extends SQLiteOpenHelper {
         contentValues.put("ImageOfExercise", editedImage);
         DB.update("Exercise", contentValues, "ExerciseIdInEDB = ?", new String[]{String.valueOf(idOfEditedInEDB)});
     }
+    // Load the data of each row
+    public Cursor getExerciseTableRows(int exerciseId){
+        Cursor rows = DB.rawQuery(" SELECT * FROM Exercise e INNER JOIN ExerciseTable t ON e.exerciseMainId = t.TableId WHERE t.TableId = ? ORDER BY t.RowNumber", new String[]{String.valueOf(exerciseId)});
+        return rows;
+    }
+    // Save data changes
+    public void updateExerciseTableRows(int exerciseId, float weight, int repetitions, float time, float distance, int order){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("RecordWeight", weight);
+        contentValues.put("RecordDistance", distance);
+        contentValues.put("RecordTime", time);
+        contentValues.put("RecordSentences", repetitions);
+        contentValues.put("TableId", exerciseId);
+        DB.execSQL("UPDATE ExerciseTable SET RecordWeight = " + weight + " , RecordDistance = " + distance + " , RecordTime = " + time+ ", RecordSentences = " + repetitions + " , TableId = " + exerciseId + " WHERE TableId = " + exerciseId + " AND RowNumber = " + order);
+    }
+    //Note when a specific exercise was trained before
+    public void setDateOfCurrentTrainingForExercise(int exerciseId, String date){
+        ContentValues contentValues = new ContentValues(1);
+        contentValues.put("DateOfLastTraining", date);
+        DB.update("Exercise", contentValues, "exerciseMainId = ?", new String[]{String.valueOf(exerciseId)});
+    }
+    //Get when a specific exercise was trained before
+    public String getDateOfExercise(int exerciseID){
+        Cursor date = DB.rawQuery("SELECT DateOfLastTraining FROM Exercise WHERE exerciseMainId = ?", new String[]{String.valueOf(exerciseID)});
+        date.moveToFirst();
+        int columnIndex = date.getColumnIndex("DateOfLastTraining");
+        return date.getString(columnIndex);
+    }
+    // New table row
+    public int addExerciseTableRow(int exerciseId, float weight, int repetitions, float time, float distance, int order) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("RecordWeight", weight);
+        contentValues.put("RecordDistance", distance);
+        contentValues.put("RecordTime", time);
+        contentValues.put("RecordSentences", repetitions);
+        contentValues.put("TableId", exerciseId);
+        contentValues.put("RowNumber", order);
+        contentValues.put("RowIsDone", 0);
+        long check = DB.insert("ExerciseTable", null, contentValues);
+        return (int) check;
+    }
+    // Mark a row as done
+    public void updateRowDone(int tableRowId, Boolean rowIsDone){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues(1);
+        contentValues.put("RowIsDone", CommonFunctions.getIntegerValueOfBoolean(rowIsDone));
+        db.update("ExerciseTable", contentValues, "tableMainId = ?", new String[]{String.valueOf(tableRowId)});
+    }
+    // Update the values of an exercise while training
+    public Boolean updateTrainingData(int planId, String planName, String planNotes, String date, String timerDuration){
+        ContentValues contentvalues = new ContentValues();
+        contentvalues.put("NameOfPlan", planName);
+        contentvalues.put("NotesForPlan", planNotes);
+        contentvalues.put("DateOfLastTraining", date);
+        contentvalues.put("TimerDurationWhenTrainingLeft", timerDuration);
+        DB.update("PlansDetails", contentvalues, "plansId = ?", new String[]{String.valueOf(planId)});
 
+        return true;
+    }
+    // Delete a row
+    public void deleteExerciseTableRow(int rowId){
+        //Select the row
+        DB.rawQuery(" Select * From ExerciseTable Where tableMainId = ? ", new String[]{String.valueOf(rowId)});
+        DB.delete("ExerciseTable", "tableMainId = ?", new String[]{String.valueOf(rowId)});
+    }
+    // Delete exercise
+    public Boolean deleteExerciseData(int exerciseId) {
+        //Select the row
+        Cursor cursor = DB.rawQuery(" Select * From Exercise Where exerciseMainId = ? ", new String[]{String.valueOf(exerciseId)});
+        long result = DB.delete("Exercise", "exerciseMainId = ?", new String[]{String.valueOf(exerciseId)});
+        if (cursor.getCount() > 0) {
+            cursor.close();
+            //If Failed: false
+            return result != -1;
+        } else {
+            cursor.close();
+            return false;
+        }
+    }
+
+    //-------------------------------------------------------------------------------
+    // More general functions
+    //-------------------------------------------------------------------------------
+    // Delete everything
+    public void completelyRemoveEntireData() {
+        DB.delete("PlansDetails", null, null);
+        DB.delete("Exercise", null, null);
+        DB.delete("ExerciseTable", null, null);
+        DB.close();
+    }
     // Function called at the end of an activity to close the database once
     public void closePlansDB(){
         DB.close();
